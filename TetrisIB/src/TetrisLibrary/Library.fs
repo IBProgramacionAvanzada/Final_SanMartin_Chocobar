fdfaf50300c40969d8ad28ddcfb892fa90eb7025
Choco97 <pablojoaquinchocobar@gmail.com>
2023-06-21 00:17:43 -0300

    Update Library.fs
    Add basic types in TetrisLibrary

--- TetrisIB/src/TetrisLibrary/Library.fs
+++ TetrisIB/src/TetrisLibrary/Library.fs
@@ -1,5 +1,44 @@
 ﻿namespace TetrisLibrary
 
-module Say =
-    let hello name =
-        printfn "Hello %s" name
+module Types = 
+    type Orientation =
+        |Up
+        |Right
+        |Down
+        |Left
+    
+    type Movement =
+        |MoveLeft
+        |MoveRight
+        |MoveDown
+        |Rotate
+        |None
+    
+    type TetrominoeType =
+        |Straight
+        |Square
+        |T
+        |L
+        |L_inverted
+        |Skew
+        |Skew_inverted
+    
+    type Tetrominoe =
+        {
+          x : int
+          y : int
+          orientation : Orientation
+          tetrominoeType : TetrominoeType
+        }
+    
+    type TetrixState =
+        {
+            board : int list list
+            score : int
+            tetrominoe : Tetrominoe
+        }
+
+module State = 
+    //let a = Types.Up
+    let createEmptyBoard (rows, columns) =
+        List.replicate rows (List.replicate columns 0)
