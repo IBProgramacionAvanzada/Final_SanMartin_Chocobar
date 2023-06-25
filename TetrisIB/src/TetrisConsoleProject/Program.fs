56f2fba5f973ca7d035b33fe125a09629d3cd7e4
Choco97 <pablojoaquinchocobar@gmail.com>
2023-06-25 17:10:21 -0300

    Update Program.fs
    Fix error in the main function of Program.fs

--- TetrisIB/src/TetrisConsoleProject/Program.fs
+++ TetrisIB/src/TetrisConsoleProject/Program.fs
@@ -1,4 +1,14 @@
 ﻿open TetrisLibrary
+open TetrisMod
 
 [<EntryPoint>]
-let tetrix = TetrisLibrary.TetrisMod.createEmptyBoard 20 10
+let main args =
+    printfn $"Bienvenido al clasico Tetris"
+    //let tetrix = createEmptyBoard (20, 10)
+    let tetrixState = {
+        board = createEmptyBoard (20, 10) ; 
+        score = 0; 
+        tetrominoe = {x = 4; y = 10; orientation = Up; tetrominoeType = Straight}
+    }
+    tetrixState |> printTetrixState
+    0
